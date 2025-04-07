6842d5f8c52c202194c00cbdfca122176d72120e
PgrEmirhan <134262825+PgrEmirhan@users.noreply.github.com>
2025-04-07 17:32:39 +0300

    Delete finalProject directory

--- finalProject/backend/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Tüm oturum verilerini temizle
-session_destroy(); // Oturumu sonlandır
-header("Location: index.php"); // Ana sayfaya yönlendir
-exit();
-?>
