535eaf36771ba6983823753c97d9f94800e5fc3c
PgrEmirhan <134262825+PgrEmirhan@users.noreply.github.com>
2025-05-29 18:39:34 +0300

    Delete backend/frontend directory

--- backend/frontend/google-login.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-$authUrl = $client->createAuthUrl();
-
-// Kullanıcıyı Google giriş sayfasına yönlendir
-header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
-exit;
-?>
