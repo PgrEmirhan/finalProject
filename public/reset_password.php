36dcfc56e9a093229f6f649076db61eed04e0184
PgrEmirhan <134262825+PgrEmirhan@users.noreply.github.com>
2025-06-24 16:52:15 +0300

    Update reset_password.php

--- public/reset_password.php
+++ public/reset_password.php
@@ -111,27 +111,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['new_password'])) {
         </div> 
             <p align="center">Tüm haklar saklıdır. TE-FS &copy2025</p>
         </footer>
-        <script>
- // Sayfa yüklendiğinde localStorage'dan dark mode'u kontrol et
-window.addEventListener('DOMContentLoaded', () => {
-  const isDarkMode = localStorage.getItem('darkMode');
-  if (isDarkMode === 'enabled') {
-    document.body.classList.add('dark-mode');
-  }
-}); 
-// Butona tıklanınca dark mode aç/kapat
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
-
-  if (document.body.classList.contains('dark-mode')) {
-    localStorage.setItem('darkMode', 'enabled'); // aktif halde sakla
-  } else {
-    localStorage.setItem('darkMode', 'disabled'); // kapalı olarak sakla
-  }
-}); 
-
-
-
+        <script src="assets/reset.js?v=1">
     </script>
 
 </body>
