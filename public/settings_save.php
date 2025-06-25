0cbd5ef0f909f88587a9f01a6e5fa6f39a374743
PgrEmirhan <134262825+PgrEmirhan@users.noreply.github.com>
2025-06-25 21:55:39 +0300

    Add files via upload

---
+++ public/settings_save.php
@@ -0,0 +1,15 @@
+  <?php
+  session_start();
+  require 'connect.php';
+  require 'auth.php';
+
+  $user_id = $_SESSION['user_id'];
+
+  $is_profile_public = isset($_POST['is_profile_public']) ? 1 : 0;
+  $is_files_public   = isset($_POST['is_files_public']) ? 1 : 0;
+  $stmt = $pdo->prepare("UPDATE users SET is_profile_public = ?, is_files_public = ?  WHERE user_id = ?");
+  $stmt->execute([$is_profile_public, $is_files_public , $user_id]);
+
+  header("Location: settings.php");
+  exit;
+  ?>
